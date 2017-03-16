a72fc7090a05ac4b0ba04516f8996e48e832f977
Omegamark <grant.mark@gmail.com>
2017-03-16 12:45:13 -0600

    added 2 views and injected an array of dogs from a custom controller

--- app.js
+++ app.js
@@ -13,10 +13,23 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: 'partial-home.html'
   })
 
+  .state('home.list', {
+    url: '/list',
+    templateUrl: 'partial-home-list.html',
+    controller: function($scope) {
+      $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+    }
+  })
+
+  .state('home.paragraph', {
+    url: '/paragraph',
+    template: 'I could use a drink.'
+  })
+
   //ABOUT PAGE AND MULTIPLE NAMED VIEWS
   .state('about', {
 
-  })
+  });
 
 
-})
+});
